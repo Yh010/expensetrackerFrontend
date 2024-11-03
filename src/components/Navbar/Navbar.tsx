eda38d3254b506bde4c14e3fca199578fd9f48f1
Yash Hegde <yash4success@gmail.com>
2024-11-03 20:29:03 +0530

    typo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link to="/landing" className="flex">
+            <Link to="/" className="flex">
               <DollarSignIcon className="h-8 w-8 text-blue-500" />
               <span className="ml-2 text-xl font-semibold text-gray-800 dark:text-white">
                 InvestTrack
